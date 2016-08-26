24678b498b35828486ec17c622dd2373b4934374
Maximiliano Curia <maxy@debian.org>
2016-08-27 00:12:30 +0200

    4:5.7.4-1 (patches unapplied)
    Imported using git-ubuntu import.

--- plugins/platforms/fbdev/fb_backend.cpp
+++ plugins/platforms/fbdev/fb_backend.cpp
@@ -88,10 +88,20 @@ void FramebufferBackend::openFrameBuffer()
     fd = open(deviceIdentifier().constData(), O_RDWR | O_CLOEXEC);
     if (fd < 0) {
         qCWarning(KWIN_FB) << "failed to open frame buffer device";
+        emit initFailed();
         return;
     }
     m_fd = fd;
-    queryScreenInfo();
+    if (!queryScreenInfo()) {
+        qCWarning(KWIN_FB) << "failed to query framebuffer information";
+        emit initFailed();
+        return;
+    }
+    initImageFormat();
+    if (m_imageFormat == QImage::Format_Invalid) {
+        emit initFailed();
+        return;
+    }
     setReady(true);
     emit screensQueried();
 }
@@ -154,8 +164,13 @@ void FramebufferBackend::unmap()
 
 QImage::Format FramebufferBackend::imageFormat() const
 {
+    return m_imageFormat;
+}
+
+void FramebufferBackend::initImageFormat()
+{
     if (m_fd < 0) {
-        return QImage::Format_Invalid;
+        return;
     }
 
     qCDebug(KWIN_FB) << "Bits Per Pixel: " << m_bitsPerPixel;
@@ -178,7 +193,7 @@ QImage::Format FramebufferBackend::imageFormat() const
             m_green.offset == 8 &&
             m_red.offset == 16) {
         qCDebug(KWIN_FB) << "Framebuffer format is RGB32";
-        return QImage::Format_RGB32;
+        m_imageFormat = QImage::Format_RGB32;
     } else if (m_bitsPerPixel == 24 &&
             m_red.length == 8 &&
             m_green.length == 8 &&
@@ -187,7 +202,8 @@ QImage::Format FramebufferBackend::imageFormat() const
             m_green.offset == 8 &&
             m_red.offset == 16) {
         qCDebug(KWIN_FB) << "Framebuffer Format is RGB888";
-        return QImage::Format_RGB888;
+        m_bgr = true;
+        m_imageFormat = QImage::Format_RGB888;
     } else if (m_bitsPerPixel == 16 &&
             m_red.length == 5 &&
             m_green.length == 6 &&
@@ -196,10 +212,9 @@ QImage::Format FramebufferBackend::imageFormat() const
             m_green.offset == 5 &&
             m_red.offset == 11) {
         qCDebug(KWIN_FB) << "Framebuffer Format is RGB16";
-        return QImage::Format_RGB16;
+        m_imageFormat = QImage::Format_RGB16;
     }
     qCWarning(KWIN_FB) << "Framebuffer format is unknown";
-    return QImage::Format_Invalid;
 }
 
 }

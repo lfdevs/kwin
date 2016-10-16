d6b2d044e6c0004d9f023aec0946b38f21c6cb86
Maximiliano Curia <maxy@debian.org>
2016-10-16 23:00:42 +0200

    4:5.8.1-1 (patches unapplied)
    Imported using git-ubuntu import.

--- autotests/integration/globalshortcuts_test.cpp
+++ autotests/integration/globalshortcuts_test.cpp
@@ -23,8 +23,13 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "platform.h"
 #include "screens.h"
 #include "shell_client.h"
+#include "useractions.h"
 #include "wayland_server.h"
 #include "workspace.h"
+
+#include <KWayland/Client/shell.h>
+#include <KWayland/Client/surface.h>
+#include <KWayland/Server/seat_interface.h>
 
 #include <KGlobalAccel>
 #include <linux/input.h>
@@ -43,6 +48,9 @@ private Q_SLOTS:
     void cleanup();
 
     void testConsumedShift();
+    void testRepeatedTrigger();
+    void testUserActionsMenu();
+    void testMetaShiftW();
 };
 
 void GlobalShortcutsTest::initTestCase()
@@ -64,12 +72,14 @@ void GlobalShortcutsTest::initTestCase()
 
 void GlobalShortcutsTest::init()
 {
+    QVERIFY(Test::setupWaylandConnection(s_socketName));
     screens()->setCurrent(0);
     KWin::Cursor::setPos(QPoint(640, 512));
 }
 
 void GlobalShortcutsTest::cleanup()
 {
+    Test::destroyWaylandConnection();
 }
 
 void GlobalShortcutsTest::testConsumedShift()
@@ -94,6 +104,96 @@ void GlobalShortcutsTest::testConsumedShift()
 
     // release shift
     kwinApp()->platform()->keyboardKeyReleased(KEY_LEFTSHIFT, timestamp++);
+}
+
+void GlobalShortcutsTest::testRepeatedTrigger()
+{
+    // this test verifies that holding a key, triggers repeated global shortcut
+    // in addition pressing another key should stop triggering the shortcut
+
+    QScopedPointer<QAction> action(new QAction(nullptr));
+    action->setProperty("componentName", QStringLiteral(KWIN_NAME));
+    action->setObjectName(QStringLiteral("globalshortcuts-test-consumed-shift"));
+    QSignalSpy triggeredSpy(action.data(), &QAction::triggered);
+    QVERIFY(triggeredSpy.isValid());
+    KGlobalAccel::self()->setShortcut(action.data(), QList<QKeySequence>{Qt::Key_Percent}, KGlobalAccel::NoAutoloading);
+    input()->registerShortcut(Qt::Key_Percent, action.data());
+
+    // we need to configure the key repeat first. It is only enabled on libinput
+    waylandServer()->seat()->setKeyRepeatInfo(25, 300);
+
+    // press shift+5
+    quint32 timestamp = 0;
+    kwinApp()->platform()->keyboardKeyPressed(KEY_WAKEUP, timestamp++);
+    kwinApp()->platform()->keyboardKeyPressed(KEY_LEFTSHIFT, timestamp++);
+    QCOMPARE(input()->keyboardModifiers(), Qt::ShiftModifier);
+    kwinApp()->platform()->keyboardKeyPressed(KEY_5, timestamp++);
+    QTRY_COMPARE(triggeredSpy.count(), 1);
+    // and should repeat
+    QVERIFY(triggeredSpy.wait());
+    QVERIFY(triggeredSpy.wait());
+    // now release the key
+    kwinApp()->platform()->keyboardKeyReleased(KEY_5, timestamp++);
+    QVERIFY(!triggeredSpy.wait(500));
+
+    kwinApp()->platform()->keyboardKeyReleased(KEY_WAKEUP, timestamp++);
+    QVERIFY(!triggeredSpy.wait(500));
+
+    // release shift
+    kwinApp()->platform()->keyboardKeyReleased(KEY_LEFTSHIFT, timestamp++);
+}
+
+void GlobalShortcutsTest::testUserActionsMenu()
+{
+    // this test tries to trigger the user actions menu with Alt+F3
+    // the problem here is that pressing F3 consumes modifiers as it's part of the
+    // Ctrl+alt+F3 keysym for vt switching. xkbcommon considers all modifiers as consumed
+    // which a transformation to any keysym would cause
+    // for more information see:
+    // https://bugs.freedesktop.org/show_bug.cgi?id=92818
+    // https://github.com/xkbcommon/libxkbcommon/issues/17
+
+    // first create a window
+    QScopedPointer<Surface> surface(Test::createSurface());
+    QScopedPointer<ShellSurface> shellSurface(Test::createShellSurface(surface.data()));
+    auto c = Test::renderAndWaitForShown(surface.data(), QSize(100, 50), Qt::blue);
+    QVERIFY(c);
+    QVERIFY(c->isActive());
+
+    quint32 timestamp = 0;
+    QVERIFY(!workspace()->userActionsMenu()->isShown());
+    kwinApp()->platform()->keyboardKeyPressed(KEY_LEFTALT, timestamp++);
+    kwinApp()->platform()->keyboardKeyPressed(KEY_F3, timestamp++);
+    kwinApp()->platform()->keyboardKeyReleased(KEY_F3, timestamp++);
+    QTRY_VERIFY(workspace()->userActionsMenu()->isShown());
+    kwinApp()->platform()->keyboardKeyReleased(KEY_LEFTALT, timestamp++);
+}
+
+void GlobalShortcutsTest::testMetaShiftW()
+{
+    // BUG 370341
+    QScopedPointer<QAction> action(new QAction(nullptr));
+    action->setProperty("componentName", QStringLiteral(KWIN_NAME));
+    action->setObjectName(QStringLiteral("globalshortcuts-test-consumed-shift"));
+    QSignalSpy triggeredSpy(action.data(), &QAction::triggered);
+    QVERIFY(triggeredSpy.isValid());
+    KGlobalAccel::self()->setShortcut(action.data(), QList<QKeySequence>{Qt::META + Qt::SHIFT + Qt::Key_W}, KGlobalAccel::NoAutoloading);
+    input()->registerShortcut(Qt::META + Qt::SHIFT + Qt::Key_W, action.data());
+
+    // press meta+shift+w
+    quint32 timestamp = 0;
+    kwinApp()->platform()->keyboardKeyPressed(KEY_LEFTMETA, timestamp++);
+    QCOMPARE(input()->keyboardModifiers(), Qt::MetaModifier);
+    kwinApp()->platform()->keyboardKeyPressed(KEY_LEFTSHIFT, timestamp++);
+    QCOMPARE(input()->keyboardModifiers(), Qt::ShiftModifier | Qt::MetaModifier);
+    kwinApp()->platform()->keyboardKeyPressed(KEY_W, timestamp++);
+    QEXPECT_FAIL("", "BUG 370341", Continue);
+    QTRY_COMPARE(triggeredSpy.count(), 1);
+    kwinApp()->platform()->keyboardKeyReleased(KEY_W, timestamp++);
+
+    // release meta+shift
+    kwinApp()->platform()->keyboardKeyReleased(KEY_LEFTSHIFT, timestamp++);
+    kwinApp()->platform()->keyboardKeyReleased(KEY_LEFTMETA, timestamp++);
 }
 
 WAYLANDTEST_MAIN(GlobalShortcutsTest)

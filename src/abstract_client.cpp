cfe1241ae622f4017c38f578c30f3ece9acb13d0
Norbert Preining <norbert@preining.info>
2021-11-10 08:39:33 +0900

    4:5.23.3-1 (patches unapplied)
    Imported using git-ubuntu import.

--- src/abstract_client.cpp
+++ src/abstract_client.cpp
@@ -2355,27 +2355,27 @@ void AbstractClient::createDecoration(const QRect &oldGeometry)
     }
     setDecoration(decoration);
     moveResize(QRect(oldGeometry.topLeft(), clientSizeToFrameSize(clientSize())));
-    updateDecorationInputShape();
 
     Q_EMIT geometryShapeChanged(this, oldGeometry);
 }
 
 void AbstractClient::destroyDecoration()
 {
-    delete m_decoration.decoration;
     setDecoration(nullptr);
-    m_decoration.inputRegion = QRegion();
+    resize(clientSizeToFrameSize(clientSize()));
 }
 
 void AbstractClient::setDecoration(KDecoration2::Decoration *decoration)
 {
-    m_decoration.decoration = decoration;
+    m_decoration.decoration.reset(decoration);
+    updateDecorationInputShape();
     Q_EMIT decorationChanged();
 }
 
 void AbstractClient::updateDecorationInputShape()
 {
     if (!isDecorated()) {
+        m_decoration.inputRegion = QRegion();
         return;
     }
 
--- src/effects/slidingpopups/slidingpopups.cpp
+++ src/effects/slidingpopups/slidingpopups.cpp
@@ -53,6 +53,11 @@ SlidingPopupsEffect::SlidingPopupsEffect()
             this, &SlidingPopupsEffect::stopAnimations);
 
     reconfigure(ReconfigureAll);
+
+    const EffectWindowList windows = effects->stackingOrder();
+    for (EffectWindow *window : windows) {
+        setupSlideData(window);
+    }
 }
 
 SlidingPopupsEffect::~SlidingPopupsEffect()
@@ -178,13 +183,13 @@ void SlidingPopupsEffect::postPaintWindow(EffectWindow *w)
             }
             m_animations.erase(animationIt);
         }
-        w->addRepaintFull();
+        effects->addRepaint(w->expandedGeometry());
     }
 
     effects->postPaintWindow(w);
 }
 
-void SlidingPopupsEffect::slotWindowAdded(EffectWindow *w)
+void SlidingPopupsEffect::setupSlideData(EffectWindow *w)
 {
     //X11
     if (m_atom != XCB_ATOM_NONE) {
@@ -203,7 +208,11 @@ void SlidingPopupsEffect::slotWindowAdded(EffectWindow *w)
         internal->installEventFilter(this);
         setupInternalWindowSlide(w);
     }
+}
 
+void SlidingPopupsEffect::slotWindowAdded(EffectWindow *w)
+{
+    setupSlideData(w);
     slideIn(w);
 }
 

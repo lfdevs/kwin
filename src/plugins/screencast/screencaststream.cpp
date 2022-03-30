bfc93559f7e5cd37e5f514dee6f0524ca757b89a
Aurélien COUDERC <coucouf@debian.org>
2022-03-30 14:06:41 +0200

    4:5.24.4-1 (patches unapplied)
    Imported using git-ubuntu import.

--- src/plugins/screencast/screencaststream.cpp
+++ src/plugins/screencast/screencaststream.cpp
@@ -482,18 +482,16 @@ void ScreenCastStream::recordCursor()
         return;
     }
 
-    struct pw_buffer *buffer = pw_stream_dequeue_buffer(pwStream);
-
-    if (!buffer) {
+    m_pendingBuffer = pw_stream_dequeue_buffer(pwStream);
+    if (!m_pendingBuffer) {
         return;
     }
 
-    struct spa_buffer *spa_buffer = buffer->buffer;
+    struct spa_buffer *spa_buffer = m_pendingBuffer->buffer;
     spa_buffer->datas[0].chunk->size = 0;
     sendCursorData(Cursors::self()->currentCursor(),
                    (spa_meta_cursor *) spa_buffer_find_meta_data (spa_buffer, SPA_META_Cursor, sizeof (spa_meta_cursor)));
-
-    tryEnqueue(buffer);
+    enqueue();
 }
 
 void ScreenCastStream::tryEnqueue(pw_buffer *buffer)
@@ -625,9 +623,18 @@ void ScreenCastStream::sendCursorData(Cursor *cursor, spa_meta_cursor *spa_meta_
     struct spa_meta_bitmap *spa_meta_bitmap = SPA_MEMBER (spa_meta_cursor,
                                                           spa_meta_cursor->bitmap_offset,
                                                           struct spa_meta_bitmap);
+    spa_meta_bitmap->format = SPA_VIDEO_FORMAT_RGBA;
+    spa_meta_bitmap->offset = sizeof(struct spa_meta_bitmap);
+    spa_meta_bitmap->size.width = std::min(m_cursor.bitmapSize.width(), image.width());
+    spa_meta_bitmap->size.height = std::min(m_cursor.bitmapSize.height(), image.height());
+    spa_meta_bitmap->stride = spa_meta_bitmap->size.width * 4;
 
     uint8_t *bitmap_data = SPA_MEMBER (spa_meta_bitmap, spa_meta_bitmap->offset, uint8_t);
-    QImage dest(bitmap_data, std::min(m_cursor.bitmapSize.width(), image.width()), std::min(m_cursor.bitmapSize.height(), image.height()), QImage::Format_RGBA8888_Premultiplied);
+    QImage dest(bitmap_data,
+                spa_meta_bitmap->size.width,
+                spa_meta_bitmap->size.height,
+                spa_meta_bitmap->stride,
+                QImage::Format_RGBA8888_Premultiplied);
     dest.setDevicePixelRatio(m_cursor.scale);
     dest.fill(Qt::transparent);
 
@@ -635,12 +642,6 @@ void ScreenCastStream::sendCursorData(Cursor *cursor, spa_meta_cursor *spa_meta_
         QPainter painter(&dest);
         painter.drawImage(QPoint(), image);
     }
-
-    spa_meta_bitmap->format = SPA_VIDEO_FORMAT_RGBA;
-    spa_meta_bitmap->offset = sizeof (struct spa_meta_bitmap);
-    spa_meta_bitmap->size.width = dest.width();
-    spa_meta_bitmap->size.height = dest.height();
-    spa_meta_bitmap->stride = dest.bytesPerLine();
 }
 
 void ScreenCastStream::setCursorMode(KWaylandServer::ScreencastV1Interface::CursorMode mode, qreal scale, const QRect &viewport)
